26f94ad0caa4c9cc537187a1a0f85d5e8298a6ed
Jay Gazula <jaygazula27@gmail.com>
2023-10-07 16:20:14 +0530

    Fixed unit tests by simply comparing generated and expected source java files

--- src/test/java/com/jgazula/easyresources/core/enhancedresourcebundle/PoetERBClassGeneratorTests.java
+++ src/test/java/com/jgazula/easyresources/core/enhancedresourcebundle/PoetERBClassGeneratorTests.java
@@ -2,34 +2,13 @@ package com.jgazula.easyresources.core.enhancedresourcebundle;
 
 import com.jgazula.easyresources.core.internal.classgeneration.ClassGeneratorConfig;
 import com.jgazula.easyresources.core.testutil.TestConstants;
-import org.junit.jupiter.api.Disabled;
+import com.jgazula.easyresources.core.testutil.TestHelper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
-import spoon.FluentLauncher;
-import spoon.reflect.code.CtAssignment;
-import spoon.reflect.code.CtBlock;
-import spoon.reflect.code.CtFieldWrite;
-import spoon.reflect.code.CtStatement;
-import spoon.reflect.code.CtVariableRead;
-import spoon.reflect.declaration.CtClass;
-import spoon.reflect.declaration.CtConstructor;
-import spoon.reflect.declaration.CtElement;
-import spoon.reflect.declaration.CtField;
-import spoon.reflect.declaration.CtMethod;
-import spoon.reflect.declaration.CtParameter;
-import spoon.reflect.declaration.ModifierKind;
-import spoon.reflect.visitor.filter.TypeFilter;
-import spoon.support.reflect.code.CtBlockImpl;
-import spoon.support.reflect.code.CtInvocationImpl;
-import spoon.support.reflect.code.CtStatementImpl;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.Collections;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,11 +16,11 @@ public class PoetERBClassGeneratorTests {
 
     private static final String TEST_KEY = "my.test.key";
     private static final String TEST_KEY_METHOD_NAME = "myTestKey";
+    private static final String RESOURCES_DIR = "poeterbclassgeneratortests";
 
     @TempDir
     Path tmpDir;
 
-    @Disabled
     @Test
     public void initializedWithConstructorAndClassFields() throws IOException {
         // given
@@ -57,43 +36,10 @@ public class PoetERBClassGeneratorTests {
         Path generatedFile = classGenerator.write(tmpDir);
 
         // then
-        CtClass<?> ctClass = getCtClass(generatedFile);
-        assertThatNecessaryClassFieldsWereAdded(ctClass);
-
-        Set<? extends CtConstructor<?>> constructors = ctClass.getConstructors();
-        assertThat(constructors).hasSize(1);
-        CtConstructor<?> constructor = constructors.iterator().next();
-
-        List<CtParameter<?>> parameters = constructor.getParameters();
-        assertThat(parameters).hasSize(1);
-        assertThat(parameters.get(0).getType().getTypeDeclaration().getSimpleName()).isEqualTo(ResourceBundle.class.getSimpleName());
-        assertThat(parameters.get(0).getSimpleName()).isEqualTo(PoetERBClassGenerator.RESOURCE_BUNDLE_VARIABLE_NAME);
-
-        assertThatNecessaryConstructorAssignmentsWereMade(constructor);
+        var expectedFile = TestHelper.getTestResourcePath(RESOURCES_DIR, "InitializedWithConstructorAndClassFields.java");
+        assertThat(TestHelper.generatedFileMatchesExpected(generatedFile, expectedFile)).isTrue();
     }
 
-    private void assertThatNecessaryClassFieldsWereAdded(CtClass<?> ctClass) {
-        List<CtField<?>> fields = ctClass.getFields();
-        assertThat(fields).hasSize(1);
-
-        CtField<?> resourceBundle = fields.get(0);
-        assertThat(resourceBundle.getModifiers()).containsExactlyInAnyOrder(ModifierKind.PRIVATE, ModifierKind.FINAL);
-        assertThat(resourceBundle.getSimpleName()).isEqualTo(PoetERBClassGenerator.RESOURCE_BUNDLE_VARIABLE_NAME);
-    }
-
-    private void assertThatNecessaryConstructorAssignmentsWereMade(CtConstructor<?> constructor) {
-        List<CtAssignment<?, ?>> assignments = constructor.getBody().getElements(new TypeFilter<>(CtAssignment.class));
-        assertThat(assignments).hasSize(1);
-
-        CtAssignment<?, ?> resourceBundleAssignment = assignments.get(0);
-        CtFieldWrite<?> resourceBundleLHS = (CtFieldWrite<?>) resourceBundleAssignment.getAssigned();
-        assertThat(resourceBundleLHS.getTarget().getType().getTypeDeclaration().getSimpleName()).isEqualTo(TestConstants.TEST_CLASS_NAME);
-        assertThat(resourceBundleLHS.getVariable().getSimpleName()).isEqualTo(PoetERBClassGenerator.RESOURCE_BUNDLE_VARIABLE_NAME);
-        CtVariableRead<?> resourceBundleRHS = (CtVariableRead<?>) resourceBundleAssignment.getAssignment();
-        assertThat(resourceBundleRHS.getVariable().getSimpleName()).isEqualTo(PoetERBClassGenerator.RESOURCE_BUNDLE_VARIABLE_NAME);
-    }
-
-    @Disabled
     @Test
     public void addMethodWithNoArgs() throws IOException {
         // given
@@ -109,43 +55,7 @@ public class PoetERBClassGeneratorTests {
         Path generatedFile = classGenerator.write(tmpDir);
 
         // then
-        CtClass<?> ctClass = getCtClass(generatedFile);
-
-        List<CtMethod<?>> methods = ctClass.getMethodsByName(TEST_KEY_METHOD_NAME);
-        assertThat(methods).hasSize(1);
-
-        CtMethod<?> method = methods.get(0);
-        assertThat(method.getModifiers()).containsExactly(ModifierKind.PUBLIC);
-        assertThat(method.getType().getSimpleName()).isEqualTo(String.class.getSimpleName());
-        assertThat(method.getSimpleName()).isEqualTo(TEST_KEY_METHOD_NAME);
-        assertThat(method.getParameters()).isEmpty();
-        assertThat(method.getThrownTypes()).isEmpty();
-
-        List<CtBlockImpl<?>> blocks = method.getBody().getElements(new TypeFilter<>(CtBlockImpl.class));
-        assertThat(blocks).hasSize(1);
-
-        List<CtStatement> statements = blocks.get(0).getStatements();
-        assertThat(statements).hasSize(2);
-
-        CtStatement firstStatement = statements.get(0);
-        assertThat(firstStatement).isOfAnyClassIn(CtInvocationImpl.class);
-        CtAssignment<?, ?> firstStatementAssignment = firstStatement
-                .getElements(new TypeFilter<>(CtAssignment.class)).get(0);
-        System.out.println(firstStatementAssignment);
-
-//        List<CtStatement> statements = ctBlock.getStatements();
-//        assertThat(statements).hasSize(2);
-    }
-
-    private CtClass<?> getCtClass(Path generatedFile) {
-        return new FluentLauncher()
-                .inputResource(generatedFile.toString())
-                .encoding(StandardCharsets.UTF_8)
-                .outputDirectory(tmpDir.toFile())
-                .buildModel()
-                .getElements(new TypeFilter<>(CtClass.class))
-                .stream()
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException(generatedFile + " does not contain a class"));
+        var expectedFile = TestHelper.getTestResourcePath(RESOURCES_DIR, "AddMethodWithNoArgs.java");
+        assertThat(TestHelper.generatedFileMatchesExpected(generatedFile, expectedFile)).isTrue();
     }
 }
--- src/test/java/com/jgazula/easyresources/core/testutil/TestHelper.java
+++ src/test/java/com/jgazula/easyresources/core/testutil/TestHelper.java
@@ -1,8 +1,17 @@
 package com.jgazula.easyresources.core.testutil;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Random;
 
 public class TestHelper {
@@ -10,6 +19,9 @@ public class TestHelper {
     private static final String ALPHABETS = "abcdefghijklmnopqrstuvwxyz";
     private static final int RANDOM_ALPHABETIC_LENGTH = 10;
     private static final int NUM_OF_PROPERTIES = 3;
+    private static final String COMMENT_PREFIX = "//";
+
+    private TestHelper() { }
 
     public static String randomAlphabetic() {
         StringBuilder sb = new StringBuilder();
@@ -27,5 +39,57 @@ public class TestHelper {
             properties.put(TestHelper.randomAlphabetic(), TestHelper.randomAlphabetic());
         }
         return Collections.unmodifiableMap(properties);
+    }
+
+    public static Path getTestResourcePath(String resourceDir, String resourceName) {
+        try {
+            var resource = File.separator + resourceDir + File.separator + resourceName;
+            return Paths.get(Objects.requireNonNull(TestHelper.class.getResource(resource)).toURI());
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static boolean generatedFileMatchesExpected(Path generated, Path expected) {
+        try (var generatedReader = Files.newBufferedReader(generated, StandardCharsets.UTF_8);
+             var expectedReader = Files.newBufferedReader(expected, StandardCharsets.UTF_8)) {
+
+            // account for the special cases where the first two lines are comments
+
+            // first line is a comment which we expect both files to have an exact match
+            var generatedFirstLine = generatedReader.readLine();
+            var expectedFirstLine = expectedReader.readLine();
+            var firstLineEqual = Objects.equals(generatedFirstLine, expectedFirstLine);
+            if (!firstLineEqual) {
+                return false;
+            }
+
+            // we only need to check that the second line is a comment
+            // since the comment is a dynamic timestamp
+            var generatedSecondLine = generatedReader.readLine();
+            var expectedSecondLine = expectedReader.readLine();
+            if (generatedSecondLine == null ||
+                    expectedSecondLine == null ||
+                    !generatedSecondLine.startsWith(COMMENT_PREFIX) ||
+                    !expectedSecondLine.startsWith(COMMENT_PREFIX)) {
+                throw new IllegalStateException("Timestamp comment is always expected");
+            }
+
+            while (true) {
+                var generatedLine = generatedReader.readLine();
+                var expectedLine = expectedReader.readLine();
+
+                if (generatedLine == null && expectedLine == null) {
+                    // end of both files
+                    break;
+                } else if (!Objects.equals(generatedLine, expectedLine)) {
+                    return false;
+                }
+            }
+
+            return true;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 }
